23c80c2eff1e1ab7558fad06ded1491212aeac6a
MimFSD <mimislam878@gmail.com>
2025-08-02 06:56:02 +0600

    all done

--- src/app/modules/auth/auth.validation.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const authValidation = z.object({
-  body: z.object({
-    email: z.string().min(1, "Email is required"),
-    password: z.string().min(1, "Password is required"),
-  }),
-});
